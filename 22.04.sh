1427eccbf37cc26df963284da02071b44b2de813
Paulo Imon <pauloimon@yahoo.com>
2023-11-16 20:51:58 -0300

    Add Ubuntu 22.04 post-installation script

---
+++ 22.04.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/bash
+
+# Update package list
+sudo apt update
+
+# Install utility packages from official repositories
+sudo apt install -y \
+    chrome-gnome-shell \
+    dconf-editor \
+    fonts-jetbrains-mono \
+    git \
+    git-flow \
+    gnome-sushi \
+    gnome-tweaks \
+    piper \
+    tilix
+
+# Install asdf CLI app from GitHub repository
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch master
+echo -e "\n# ---\n# asdf\n# ---" >> ~/.bashrc
+echo ". \"\$HOME/.asdf/asdf.sh\"" >> ~/.bashrc
+echo ". \"\$HOME/.asdf/completions/asdf.bash\"" >> ~/.bashrc
+
+sudo snap install \
+    beekeeper-studio \
+    bitwarden \
+    brave \
+    code \
+    spotify
+
+# Upgrade system and remove unused packages
+sudo apt dist-upgrade -y
+sudo apt autoclean && sudo apt autoremove -y
+
+# Enable system firewall
+sudo ufw enable
+
+# Customize Gnome Shell settings
+gsettings set org.gnome.desktop.background picture-options "none"
+gsettings set org.gnome.desktop.background primary-color "#000000"
+gsettings set org.gnome.desktop.default-applications.terminal exec "tilix"
+gsettings set org.gnome.desktop.interface clock-show-weekday "true"
+gsettings set org.gnome.desktop.interface locate-pointer "true"
+gsettings set org.gnome.desktop.interface show-battery-percentage "true"
+gsettings set org.gnome.desktop.peripherals.mouse natural-scroll "true"
+gsettings set org.gnome.desktop.wm.keybindings switch-panels "[]"
+gsettings set org.gnome.desktop.wm.keybindings switch-panels-backward "[]"
+gsettings set org.gnome.mutter center-new-windows "true"
+gsettings set org.gnome.nautilus.preferences open-folder-on-dnd-hover "true"
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action "minimize"
+gsettings set org.gnome.shell.keybindings toggle-overview "['<Control><Alt>Tab']"
+
+# Customize Tilix settings
+gsettings set com.gexperts.Tilix.Settings terminal-title-style "none"
+
+echo "Done!"
