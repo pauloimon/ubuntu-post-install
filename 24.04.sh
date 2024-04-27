b33ba7b76343b2ce2acebb2ceb2acf7acca16d13
Paulo Imon <3463932+pauloimon@users.noreply.github.com>
2024-04-26 22:49:24 -0300

    Add 24.04.sh

---
+++ 24.04.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+sudo apt update
+sudo apt install -y \
+  brave-browser \
+  code \
+  curl \
+  dconf-editor \
+  fonts-jetbrains-mono \
+  git \
+  git-flow \
+  gnome-sushi \
+  gnome-tweaks \
+  spotify-client
+
+sudo ufw enable
+
+echo "Done!"
